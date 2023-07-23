5fb2b3c96c8009955c3480692a1a1a81a2e0b8c9
ToukirIslamShovo <shovoodev@gmail.com>
2023-07-23 19:03:50 +0200

    create new post component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Single from "./pages/single/Single";
+import Write from "./pages/single/write/Write";
 import Topbar  from "./topbar/Topbar";
 function App() {
   return (
     <>
     <Topbar/>
-    <Single/>
+    <Write/>
     
   </>
   );
